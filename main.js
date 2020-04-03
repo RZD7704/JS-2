8444ac178df65d753186cd1bbd83a01d724de70e
Artem <artemius2014poltava@gmail.com>
2020-04-03 17:08:51 +0300

    2 lesson

---
+++ main.js
@@ -0,0 +1,52 @@
+// Условия
+// if (num < 49) {
+//     console.log('Not')
+// } else if (num > 100) {
+//     console.log('many')
+// } else {
+//     console.log('fain')
+// };
+
+// (num == 200) ? console.log('fain') : console.log('not');
+
+// switch (num) {
+//     case num < 49:
+//         console.log('not');
+//         break;
+//     case num > 80:
+//         console.log('many');
+//         break;
+//     case num > 50:
+//         console.log('fine');
+//         break;
+//     case 50:
+//         console.log('good');
+//         break;
+//     default:
+//         alert('buy');
+//         break;
+// }
+
+
+// Цыклы
+
+// let num = 1;
+
+// while (num < 8) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// do {
+//     console.log(num);
+//     num++;
+
+// } while (num < 8);
+
+// for (let i = 1; i < 8; i++) {
+//     if (i == 6) {
+//         continue
+//     }
+//     console.log(i);
+// }
